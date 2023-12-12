9803b6f21f2d42391f90c96950d051193fd14537
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-12-12 16:22:17 +0300

    websocket init

---
+++ src/main/java/com/harun/liveSlideServer/LiveSlideServerApplication.java
@@ -0,0 +1,13 @@
+package com.harun.liveSlideServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveSlideServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveSlideServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harun/liveSlideServer/LiveSlideServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harun.liveSlideServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveSlideServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
